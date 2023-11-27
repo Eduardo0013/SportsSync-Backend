ac9f4a9d335871c03a07faa9814784215a97fe55
Eduardo Salas Code <thewarrior20013@gmail.com>
2023-11-26 18:39:21 -0600

    prueba 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Sanctum::ignoreMigrations();
-        app()->singleton(
-            App\Http\Service\AuthService::class
-        );
+        
     }
 
     /**
